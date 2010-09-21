a28ac498f7422924cedb382a351836ba7732114a
yku <kz2yz4@gmail.com>
2010-09-22 01:15:51 +0900

    rewrite event_search function

--- atnd.py
+++ atnd.py
@@ -14,49 +14,44 @@ class Atnd:
         url = 'http://api.atnd.org/events/?%s' % (self.prepare_query(param))
         print url
         doc = xml.dom.minidom.parseString(urllib2.urlopen(url).read())
-        
-        results = doc.getElementsByTagName('results_returned')[0].firstChild.data
-        event_id = doc.getElementsByTagName('event_id')
-        title = doc.getElementsByTagName('title')
-        catche = doc.getElementsByTagName('catch')
-        description = doc.getElementsByTagName('description')
-        event_url = doc.getElementsByTagName('event_url')
-        started_at = doc.getElementsByTagName('started_at')
-        ended_at = doc.getElementsByTagName('ended_at')
-        url = doc.getElementsByTagName('url')
-        limit = doc.getElementsByTagName('limit')
-        addresse = doc.getElementsByTagName('address')
-        place = doc.getElementsByTagName('place')
-        lat = doc.getElementsByTagName('lat')
-        lon = doc.getElementsByTagName('lon')
-        owner_id = doc.getElementsByTagName('owner_id')
-        owner_nickname = doc.getElementsByTagName('owner_nickname')
-        owner_twitter_id = doc.getElementsByTagName('owner_twitter_id')
-        accepted = doc.getElementsByTagName('accepted')
-        waiting = doc.getElementsByTagName('waiting')
-        updated_at = doc.getElementsByTagName('updated_at')
+        root = doc.documentElement 
+        events_dom = root.getElementsByTagName('event')
+    
+        ret = {}
+        ret['results_returned'] = root.getElementsByTagName('results_returned')[0].firstChild.data
+        ret['results_start'] = root.getElementsByTagName('results_start')[0].firstChild.data
+    
+        event_structure = (
+                           'event_id',
+                           'title',
+                           'catch',
+                           'description',
+                           'event_url',
+                           'started_at',
+                           'ended_at',
+                           'url',
+                           'limit',
+                           'address',
+                           'place',
+                           'lat',
+                           'lon',
+                           'owner_id',
+                           'owner_nickname',
+                           'owner_twitter_id',
+                           'accepted',
+                           'waiting',
+                           'updated_at')
+        event = []
+        for e in events_dom:
+            data = {}
+            for tag in event_structure:
+                elem = e.getElementsByTagName(tag)[0]
+                if elem.hasChildNodes():
+                    data[tag] = elem.firstChild.data
+            event.append(data)
 
-        ret = [] 
-        for i in xrange(0, int(results)):
-            ret.append({'event_id':event_id[i].firstChild.data,
-                        'title':title[i].firstChild.data,
-                        'catch':catche[i].firstChild.data,
-                        'description':description[i].firstChild.data,
-                        'event_url':event_url[i].firstChild.data,
-                        'started_at':started_at[i].firstChild.data,
-                        'ended_at':ended_at[i].firstChild.data,
-                        'url':url[i].firstChild.data,
-                        'limit':limit[i].firstChild.data,
-                        'address':addresse[i].firstChild.data,
-                        'place':place[i].firstChild.data,
-                        'lat':lat[i].firstChild.data,
-                        'lon':lon[i].firstChild.data,
-                        'owner_id':owner_id[i].firstChild.data,
-                        'owner_twitter_id':'' if owner_twitter_id[i].getAttribute('nil') else owner_twitter_id[i].firstChild.data,
-                        'accepted':accepted[i].firstChild.data,
-                        'waiting':waiting[i].firstChild.data,
-                        'updated_at':updated_at[i].firstChild.data,
-                        })
+        ret['event'] = event
+        
         return ret 
  
     def event_user_search(self, param={}):
@@ -127,8 +122,8 @@ param = { 'twitter_id':'yku_',
           'count': '1'
         }
 param = { 'twitter_id':'yku_', 'count':'3' }
-result = api.event_user_search(param)
-for r in result:
+result = api.event_search(param)
+for r in result['event']:
     for k, v in r.items():
         print k, v
     print 
